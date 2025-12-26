25418b9b71c2a733f93c93dbe7226868b489e21d
heewonlee <66260965+heewonle@users.noreply.github.com>
2025-12-26 09:12:10 +0900

    [level 1] Title: 내적, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.cpp
+++ 프로그래머스/1/70128. 내적/내적.cpp
@@ -1,13 +1,7 @@
-#include <string>
 #include <vector>
-
+#include <numeric>
 using namespace std;
 
-int solution(vector<int> a, vector<int> b) {
-    int answer = 0;
-    for(int i=0; i<a.size(); i++){
-        answer+= a[i]*b[i];
-    }
-    
-    return answer;
+int solution(vector<int> a, vector<int> b) {    
+    return inner_product(a.begin(),a.end(),b.begin(),0);
 }
