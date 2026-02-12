99b7373b3d4e35fde2a907c7934c543b46eb08b8
heewonlee <66260965+heewonle@users.noreply.github.com>
2026-02-12 09:35:03 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
@@ -1,19 +1,8 @@
 #include <string>
 #include <vector>
-#include <set>
-#include <algorithm>
-#include <iostream>
+#include <numeric>
 using namespace std;
-int gcd(int a, int b){
-    int max_value = max(a,b);
-    int min_value = min(a,b);
-    while(max_value%min_value!=0){
-        int least = max_value % min_value;
-        max_value = min_value;
-        min_value = least;
-    }
-    return min_value;
-}
+
 int solution(vector<int> arr) {
     int answer = arr[0];
     for(int j=1; j<arr.size(); j++){
