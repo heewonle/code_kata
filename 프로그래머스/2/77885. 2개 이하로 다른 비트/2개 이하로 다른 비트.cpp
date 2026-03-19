f66a655d62d4e023116eabd5021c9d2424667caa
heewonlee <66260965+heewonle@users.noreply.github.com>
2026-03-19 09:49:57 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 29.21 ms, Memory: 27.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+vector<long long> solution(vector<long long> numbers) {
+    vector<long long> answer;
+    for (long long x : numbers) {
+        if (x % 2 == 0) {
+            answer.push_back(x + 1);
+        } else {
+            answer.push_back(x + 1 + ((x ^ (x + 1)) >> 2));
+        }
+    }
+    return answer;
+}
