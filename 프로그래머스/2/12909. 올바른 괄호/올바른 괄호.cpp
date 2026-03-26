d7d6eda0e7228bdb0f5758273f02f3550426dade
heewonlee <66260965+heewonle@users.noreply.github.com>
2026-03-26 09:48:58 +0900

    [level 2] Title: 올바른 괄호, Time: 0.47 ms, Memory: 3.88 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,23 @@
+#include<string>
+#include<stack>
+#include <iostream>
+
+using namespace std;
+
+bool solution(string s)
+{
+    stack<char> st;
+    for(char c : s){
+        if(c=='('){
+            st.push(c);
+        }
+        else{
+            if(st.empty()){
+                return false;
+            }
+            st.pop();
+        }
+    }
+    
+    return st.empty();
+}
