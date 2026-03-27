f947de57b3fbe3e9a9bc0ac2e93ddcd52163b1a9
heewonlee <66260965+heewonle@users.noreply.github.com>
2026-03-27 09:41:29 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -1,38 +1,39 @@
 #include <string>
 #include <vector>
+#include <queue>
 #include <cmath>
-#include <unordered_map>
 #include <iostream>
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
-    vector<int> before;
-    for(int i=0; i<progresses.size(); i++){
-    double day = (100 - progresses[i]) / (double)speeds[i];
-    int result = (int)ceil(day);
-        before.push_back(result);
+    queue<int> pq;
+
+    for (int i = 0; i < progresses.size(); i++) {
+        int days = ceil((float)(100 - progresses[i]) / speeds[i]);
+        pq.push(days);
     }
-    int bf_day = before[0];
-    unordered_map<int,int> counts;
-    vector<int> days;
-    days.push_back(bf_day);
-    counts[bf_day] = 0;
-    for(int i=1; i<before.size(); i++){
-        if(before[i]<=bf_day){
-            before[i] = bf_day;
+
+    if (pq.empty()) return answer;
+
+    int before = pq.front();
+    int count = 0;
+
+    while (!pq.empty()) {
+        int t = pq.front();
+
+        if (t <= before) {
+            count++;
+        } else {
+            answer.push_back(count);
+            before = t;
+            count = 1;
         }
-        else{
-            bf_day = before[i];
-            days.push_back(bf_day);
-            counts[bf_day] =0;
-        }
+
+        pq.pop();
     }
-    for(int i=0; i<before.size(); i++){
-        counts[before[i]] +=1;
-    }
-    for(int d : days){
-        answer.push_back(counts[d]);
-    }
+
+    answer.push_back(count);
+
     return answer;
 }
