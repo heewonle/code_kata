ae6f0dcbc144f8395695a1eadfdbd8bf89410edb
heewonlee <66260965+heewonle@users.noreply.github.com>
2026-05-27 09:04:31 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 3.93 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int answer = 0;
+    sort(d.begin(),d.end());
+    for(int i : d){
+        if(budget-i>=0){
+            budget -=i;
+            answer++;
+        }
+    }
+    return answer;
+}
